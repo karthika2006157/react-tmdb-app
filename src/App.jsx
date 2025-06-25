e85fc7f2b5f6d7660d5034217de02b7c7aa8be2e
karthikabalasubramani75@gmail.com <karthikabalasubramani75@gmail.com>
2025-06-25 17:16:25 +0530

    complete frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react"
+import { useDebounce } from "react-use";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
-const API_BASE_URL = "https://api.themoviedb.org/3"
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const API_OPTIONS = {
@@ -13,33 +17,84 @@ const API_OPTIONS = {
 }
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMoviesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debounceSearchTerm, setDebounceSearchTerm] = useState("");
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDebounceSearchTerm(searchTerm), 1000, [searchTerm]);
+
+  const fetchMovies = async (query = "") => {
+    setIsLoading(true);
+    setErrorMessage("");
+
   try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      //throw new Error("Failed to fetch movies");
+      const endpoint = query
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response) {
+        throw new Error("Failed to fetch movies");
+      }
+
       const data = await response.json();
+
+      if(data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMoviesList([]);
+        return;
+      }
+      setMoviesList(data.results || []);
+
       console.log(data);
   } catch (error) {
-    console.log(`Error fetching movie: ${error}`);
+    console.error(`Error fetching movie: ${error}`);
+    setErrorMessage("Error fetching movies. Please try again later.");
+  } finally {
+    setIsLoading(false);
   }
 };
 
 useEffect(() => {
-  fetchMovies();
-}, []);
+  fetchMovies(debounceSearchTerm);
+}, [debounceSearchTerm]);
+
   return (
     <main>
       <div className="pattern"/>
       <div className="wrapper">
         <header>
           <img src="hero.png" alt="" />
-          <h1>Find <span className="text-purple-500">Movies!</span> Hit Play <span className="text-gradient">Feel everything</span></h1>
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <h1>Find <span className="text-purple-500">Movies!
+            <br/>
+            </span> Hit Play <span className="text-gradient">Feel everything</span></h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {
+            isLoading ?
+              <Spinner/>
+            :
+              (
+                <ul>
+                  {
+                    movieList.map((movie) => (
+                      <MovieCard key={movie.id} movie={movie}/>
+                    ))
+                  }
+                </ul>
+              )
+          }
+      
+        {errorMessage && <p className="text-red-500">{errorMessage}</p> }
+        </section>
       </div>
     </main>
-  )  
+  );
 }
+
 export default App
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,17 @@ const Search = ({searchTerm, setSearchTerm}) => {
   return (
     <div className="search">
         <div>
-        <img src="search.svg" alt="search" />
-        <input type="text"
-        placeholder="Search Movies"
-        values={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-        />
+            <img src="search.svg" alt="" />
+            <input type="text" 
+                placeholder="Search Movies"
+                values={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+            />
+        </div>
     </div>
-</div>
   )
 }
 
 export default Search
+ 
+//rafce
